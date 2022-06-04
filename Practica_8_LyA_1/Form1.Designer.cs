34fb0eca78768772b094fc1358bf614bd716bb79
Víctor Hugo Carreón Pulido <hv_carreon@hotmail.com>
2022-06-03 21:23:02 -0500

    Agregar archivos de proyecto.

---
+++ Practica_8_LyA_1/Form1.Designer.cs
@@ -0,0 +1,287 @@
+﻿namespace Practica_8_LyA_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbCntrl = new System.Windows.Forms.TabControl();
+            this.tbPgGenerador = new System.Windows.Forms.TabPage();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rdbtnNA = new System.Windows.Forms.RadioButton();
+            this.rdbtnEjer4 = new System.Windows.Forms.RadioButton();
+            this.rdbtnEjer3 = new System.Windows.Forms.RadioButton();
+            this.rdbtnEjer2 = new System.Windows.Forms.RadioButton();
+            this.rdbtnEjer1 = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnGenerar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.lstResultados = new System.Windows.Forms.ListBox();
+            this.txtRegex = new System.Windows.Forms.TextBox();
+            this.tbPgComprobador = new System.Windows.Forms.TabPage();
+            this.tbPgDocumentacion = new System.Windows.Forms.TabPage();
+            this.tbPgInfo = new System.Windows.Forms.TabPage();
+            this.tbCntrl.SuspendLayout();
+            this.tbPgGenerador.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbCntrl
+            // 
+            this.tbCntrl.Controls.Add(this.tbPgGenerador);
+            this.tbCntrl.Controls.Add(this.tbPgComprobador);
+            this.tbCntrl.Controls.Add(this.tbPgDocumentacion);
+            this.tbCntrl.Controls.Add(this.tbPgInfo);
+            this.tbCntrl.Location = new System.Drawing.Point(12, 12);
+            this.tbCntrl.Name = "tbCntrl";
+            this.tbCntrl.SelectedIndex = 0;
+            this.tbCntrl.Size = new System.Drawing.Size(776, 417);
+            this.tbCntrl.TabIndex = 0;
+            // 
+            // tbPgGenerador
+            // 
+            this.tbPgGenerador.Controls.Add(this.groupBox1);
+            this.tbPgGenerador.Controls.Add(this.label2);
+            this.tbPgGenerador.Controls.Add(this.btnGenerar);
+            this.tbPgGenerador.Controls.Add(this.label1);
+            this.tbPgGenerador.Controls.Add(this.numericUpDown1);
+            this.tbPgGenerador.Controls.Add(this.lstResultados);
+            this.tbPgGenerador.Controls.Add(this.txtRegex);
+            this.tbPgGenerador.Location = new System.Drawing.Point(4, 22);
+            this.tbPgGenerador.Name = "tbPgGenerador";
+            this.tbPgGenerador.Padding = new System.Windows.Forms.Padding(3);
+            this.tbPgGenerador.Size = new System.Drawing.Size(768, 391);
+            this.tbPgGenerador.TabIndex = 0;
+            this.tbPgGenerador.Text = "Generador";
+            this.tbPgGenerador.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rdbtnNA);
+            this.groupBox1.Controls.Add(this.rdbtnEjer4);
+            this.groupBox1.Controls.Add(this.rdbtnEjer3);
+            this.groupBox1.Controls.Add(this.rdbtnEjer2);
+            this.groupBox1.Controls.Add(this.rdbtnEjer1);
+            this.groupBox1.Location = new System.Drawing.Point(15, 109);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(303, 99);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Precargar Expresiones";
+            // 
+            // rdbtnNA
+            // 
+            this.rdbtnNA.AutoSize = true;
+            this.rdbtnNA.Location = new System.Drawing.Point(207, 29);
+            this.rdbtnNA.Name = "rdbtnNA";
+            this.rdbtnNA.Size = new System.Drawing.Size(65, 17);
+            this.rdbtnNA.TabIndex = 4;
+            this.rdbtnNA.TabStop = true;
+            this.rdbtnNA.Text = "Ninguno";
+            this.rdbtnNA.UseVisualStyleBackColor = true;
+            this.rdbtnNA.Click += new System.EventHandler(this.rdbtnNA_Click);
+            // 
+            // rdbtnEjer4
+            // 
+            this.rdbtnEjer4.AutoSize = true;
+            this.rdbtnEjer4.Location = new System.Drawing.Point(116, 52);
+            this.rdbtnEjer4.Name = "rdbtnEjer4";
+            this.rdbtnEjer4.Size = new System.Drawing.Size(74, 17);
+            this.rdbtnEjer4.TabIndex = 3;
+            this.rdbtnEjer4.TabStop = true;
+            this.rdbtnEjer4.Text = "Ejercicio 4";
+            this.rdbtnEjer4.UseVisualStyleBackColor = true;
+            this.rdbtnEjer4.Click += new System.EventHandler(this.rdbtnEjer4_Click);
+            // 
+            // rdbtnEjer3
+            // 
+            this.rdbtnEjer3.AutoSize = true;
+            this.rdbtnEjer3.Location = new System.Drawing.Point(116, 29);
+            this.rdbtnEjer3.Name = "rdbtnEjer3";
+            this.rdbtnEjer3.Size = new System.Drawing.Size(74, 17);
+            this.rdbtnEjer3.TabIndex = 2;
+            this.rdbtnEjer3.TabStop = true;
+            this.rdbtnEjer3.Text = "Ejercicio 3";
+            this.rdbtnEjer3.UseVisualStyleBackColor = true;
+            this.rdbtnEjer3.Click += new System.EventHandler(this.rdbtnEjer3_Click);
+            // 
+            // rdbtnEjer2
+            // 
+            this.rdbtnEjer2.AutoSize = true;
+            this.rdbtnEjer2.Location = new System.Drawing.Point(15, 52);
+            this.rdbtnEjer2.Name = "rdbtnEjer2";
+            this.rdbtnEjer2.Size = new System.Drawing.Size(74, 17);
+            this.rdbtnEjer2.TabIndex = 1;
+            this.rdbtnEjer2.TabStop = true;
+            this.rdbtnEjer2.Text = "Ejercicio 2";
+            this.rdbtnEjer2.UseVisualStyleBackColor = true;
+            this.rdbtnEjer2.Click += new System.EventHandler(this.rdbtnEjer2_Click);
+            // 
+            // rdbtnEjer1
+            // 
+            this.rdbtnEjer1.AutoSize = true;
+            this.rdbtnEjer1.Location = new System.Drawing.Point(15, 29);
+            this.rdbtnEjer1.Name = "rdbtnEjer1";
+            this.rdbtnEjer1.Size = new System.Drawing.Size(74, 17);
+            this.rdbtnEjer1.TabIndex = 0;
+            this.rdbtnEjer1.TabStop = true;
+            this.rdbtnEjer1.Text = "Ejercicio 1";
+            this.rdbtnEjer1.UseVisualStyleBackColor = true;
+            this.rdbtnEjer1.Click += new System.EventHandler(this.rdbtnEjer1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(333, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Resultados";
+            // 
+            // btnGenerar
+            // 
+            this.btnGenerar.Location = new System.Drawing.Point(15, 65);
+            this.btnGenerar.Name = "btnGenerar";
+            this.btnGenerar.Size = new System.Drawing.Size(90, 23);
+            this.btnGenerar.TabIndex = 4;
+            this.btnGenerar.Text = "Generar";
+            this.btnGenerar.UseVisualStyleBackColor = true;
+            this.btnGenerar.Click += new System.EventHandler(this.btnGenerar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Expresión Regular";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(258, 68);
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(60, 20);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // lstResultados
+            // 
+            this.lstResultados.FormattingEnabled = true;
+            this.lstResultados.Location = new System.Drawing.Point(336, 39);
+            this.lstResultados.Name = "lstResultados";
+            this.lstResultados.Size = new System.Drawing.Size(414, 329);
+            this.lstResultados.TabIndex = 1;
+            // 
+            // txtRegex
+            // 
+            this.txtRegex.Location = new System.Drawing.Point(15, 39);
+            this.txtRegex.Name = "txtRegex";
+            this.txtRegex.Size = new System.Drawing.Size(303, 20);
+            this.txtRegex.TabIndex = 0;
+            // 
+            // tbPgComprobador
+            // 
+            this.tbPgComprobador.Location = new System.Drawing.Point(4, 22);
+            this.tbPgComprobador.Name = "tbPgComprobador";
+            this.tbPgComprobador.Padding = new System.Windows.Forms.Padding(3);
+            this.tbPgComprobador.Size = new System.Drawing.Size(768, 391);
+            this.tbPgComprobador.TabIndex = 1;
+            this.tbPgComprobador.Text = "Comprobar";
+            this.tbPgComprobador.UseVisualStyleBackColor = true;
+            // 
+            // tbPgDocumentacion
+            // 
+            this.tbPgDocumentacion.Location = new System.Drawing.Point(4, 22);
+            this.tbPgDocumentacion.Name = "tbPgDocumentacion";
+            this.tbPgDocumentacion.Padding = new System.Windows.Forms.Padding(3);
+            this.tbPgDocumentacion.Size = new System.Drawing.Size(768, 391);
+            this.tbPgDocumentacion.TabIndex = 2;
+            this.tbPgDocumentacion.Text = "Documentación";
+            this.tbPgDocumentacion.UseVisualStyleBackColor = true;
+            // 
+            // tbPgInfo
+            // 
+            this.tbPgInfo.Location = new System.Drawing.Point(4, 22);
+            this.tbPgInfo.Name = "tbPgInfo";
+            this.tbPgInfo.Padding = new System.Windows.Forms.Padding(3);
+            this.tbPgInfo.Size = new System.Drawing.Size(768, 391);
+            this.tbPgInfo.TabIndex = 3;
+            this.tbPgInfo.Text = "Información ";
+            this.tbPgInfo.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tbCntrl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tbCntrl.ResumeLayout(false);
+            this.tbPgGenerador.ResumeLayout(false);
+            this.tbPgGenerador.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tbCntrl;
+        private System.Windows.Forms.TabPage tbPgGenerador;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rdbtnNA;
+        private System.Windows.Forms.RadioButton rdbtnEjer4;
+        private System.Windows.Forms.RadioButton rdbtnEjer3;
+        private System.Windows.Forms.RadioButton rdbtnEjer2;
+        private System.Windows.Forms.RadioButton rdbtnEjer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnGenerar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.ListBox lstResultados;
+        private System.Windows.Forms.TextBox txtRegex;
+        private System.Windows.Forms.TabPage tbPgComprobador;
+        private System.Windows.Forms.TabPage tbPgDocumentacion;
+        private System.Windows.Forms.TabPage tbPgInfo;
+    }
+}
+
---
+++ Practica_8_LyA_1/Form1.cs
@@ -0,0 +1,79 @@
+﻿using Fare;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica_8_LyA_1
+{
+    public partial class Form1 : Form
+    {
+        Xeger obj;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void rdbtnEjer1_Click(object sender, EventArgs e)
+        {
+            this.txtRegex.Text = @"^[1-9]+[02468]$";
+            this.obj = new Xeger(@"^[1-9]+[02468]$");
+        }
+
+        private void rdbtnEjer2_Click(object sender, EventArgs e)
+        {
+            this.txtRegex.Text = @"^[aeiuoAEIOU][1-9]+[A]$";
+            this.obj = new Xeger(@"^[aeiuoAEIOU][1-9]+[A]$");
+        }
+
+        private string[] GenerarTextos(Xeger expresion, int cantidad) 
+        {
+            string[] textos = new string[cantidad];
+
+            for (int i = 0; i < cantidad; i++)
+            {
+                textos[i] = expresion.Generate();
+            }
+
+            //Array.ForEach(textos, x => x = expresion.Generate());
+            return textos;
+        }
+
+        private void RellenarLista(string[] arreglo) 
+        {
+            for (int i = 0; i < arreglo.Length; i++)
+                this.lstResultados.Items.Add(arreglo[i]);
+
+        }
+
+        private void rdbtnEjer3_Click(object sender, EventArgs e)
+        {
+            this.txtRegex.Text = @"^[AB]{2,3}[CD]{3,4}[DE]{4,5}$";
+            this.obj = new Xeger(@"^[AB]{2,3}[CD]{3,4}[DE]{4,5}$");
+        }
+
+        private void rdbtnEjer4_Click(object sender, EventArgs e)
+        {
+            this.txtRegex.Text = @"^[a-zA-Z]+[a-zA-Z\w_-]+@[a-zA-Z0-9]+(\.(com){1})(\.(mx)){0,1}$";
+            this.obj = new Xeger(@"^[a-zA-Z]+[a-zA-Z\w_-]+@[a-zA-Z0-9]+(\.(com){1})(\.(mx)){0,1}$");
+        }
+
+        private void btnGenerar_Click(object sender, EventArgs e)
+        {
+            lstResultados.Items.Clear();
+            RellenarLista(GenerarTextos(this.obj, (int)numericUpDown1.Value));
+            Console.WriteLine("Generado");
+        }
+
+        private void rdbtnNA_Click(object sender, EventArgs e)
+        {
+            this
+        }
+    }
+}
